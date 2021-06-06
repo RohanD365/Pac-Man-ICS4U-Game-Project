128519547004360deaf9368c0c8e052f2a7ad444
rajdo <rajdo@10.0.0.13>
2021-06-05 21:02:09 -0400

    ICS4U Game Project

---
+++ src/game/Game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements ActionListener{
+    
+    
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
